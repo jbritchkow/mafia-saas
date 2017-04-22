2b9dc05f75cf92746ee9075cd1ea5545cdad4558
AMANDA\Amanda <aslee1@crimson.ua.edu>
2017-04-22 10:27:41 -0600

    First bit of code/first attempt
    trying to commit for first time.

---
+++ CloudMafia.java
@@ -0,0 +1,15 @@
+public class CloudMafia {
+    public static void main(String args[]){
+        //hardcoded the code. I don't think we should need to worry about
+        //randomizing it too much? In any case, used an int because ints
+        //are easy to compare and easy to randomize.
+        int code =15323;
+        System.out.println(code+"");
+        //main as controller class?
+        //sockets
+        //if code, launch thread
+        //once all threads are launched, assign roles
+
+    }
+
+}
