82867c3b5f3023de07a6382133417507886dbb57
AMANDA\Amanda <aslee1@crimson.ua.edu>
2017-04-22 17:31:40 -0600

    Added all states and todos for real

--- src/MafiaGame.java
+++ src/MafiaGame.java
@@ -6,6 +6,8 @@ public class MafiaGame {
     private static final int ASKEDCODE = 1;
     private static final int ASKEDNAME = 2;
     private static final int GOTNAME = 3;
+    private static final int STEPONE=4;
+
     private int state =GAMESTART;
 
     //private int code =0;
@@ -34,6 +36,18 @@ public class MafiaGame {
             else{
                 state=GAMESTART;
             }
+        }
+        else if (state ==GOTNAME){
+            //TODO: Mafia kill, police guess, doctor heals
+            //TODO: Civilians chill
+            //TODO: Wait until stage is over, then unlock and move to next stage
+            state =STEPONE;
+        }
+        else if (state ==STEPONE){
+            //TODO: voting
+            //TODO: Display results; show if sucessful or not
+            //TODO: If all mafia dead, gameOutput=Game Over
+            //TODO: else, set state back to gotname and restart
         }
         return gameOutput;
     }
